e9f7a969b81a96195b55c6327b97fb1c5b563a6c
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-13 10:35:47 -0500

    adding pug templates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 // import './../assets/sass/main.scss';
 import 'bootstrap';
+
+const pug = require('pug');
+
+const compiledFunction = pug.compileFile('index.pug');
+const data = {};
+
+const rootNode = document.getElementById('root'); 
+
+if (rootNode) {
+    rootNode.innerHTML = compiledFunction(data);
+}
