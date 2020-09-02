287aee5feb8b6b09abcb1b2fe51294633f5fccad
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-09-02 13:49:58 -0400

    added babel, sticky nav

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,16 @@ module.exports = (env, options) => {
                 ],
         module: {
             rules: [{
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                  },
+                  {
                     test: /\.(scss)$/,
                     use: [
                         /*{
